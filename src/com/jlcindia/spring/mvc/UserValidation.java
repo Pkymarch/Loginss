77390ab428be87ccb95b9ab1aafa12205e8abf53
Pkymarch <62890988+Pkymarch@users.noreply.github.com>
2020-04-03 20:47:12 +0530

    Add files via upload

---
+++ src/com/jlcindia/spring/mvc/UserValidation.java
@@ -0,0 +1,29 @@
+package com.jlcindia.spring.mvc;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+
+@Component
+public class UserValidation implements Validator {
+
+	@Override
+	public boolean supports(Class clazz) {
+		return User.class.equals(clazz);
+	}
+
+	@Override
+	public void validate(Object command, Errors errors) {
+		User user = (User) command;
+		String un=user.getUsername();
+		String pw=user.getPassword();
+		if (un.length() == 0 || un == null) {
+			errors.rejectValue("username", "error.username.reuired", null, "Username Mandatory.");
+		}
+		if (pw == null || pw.length() == 0) {
+			errors.rejectValue("password", "error.password.reuired", null, "Password Mandatory.");
+		}
+	}
+
+}
